94bddcebf34fc489fbaf8972c55985e98e3a2e87
MahmoudHamdy <hamdy.xd55@gmail.com>
2023-10-18 01:56:32 +0300

    Fix square class that takes a width and a height

--- 0x01-challenge/README.md
+++ 0x01-challenge/README.md
@@ -1,1 +1,12 @@
-# Fix-my-code-1
+# Project: 0x01. Fix my code
+
+## Tasks
+
+| Task | File |
+| ---- | ---- |
+| 0. Server status | [status_server/](./status_server/) |
+| 1. My square | [square.py](./square.py) |
+| 2. Step 2: User model | [user.py](./user.py) |
+| 3. Blog access | [blog](./blog) |
+| 4. Never leave the office | [react-blog](./react-blog) |
+
--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 
-class square():
+class Square():
+    """Class Square"""
     
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """init function"""
+        self.width = self.height = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +18,16 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        """ Permiter of the square """
+        return (self.width * 2) + (self.width * 2)
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        """string representaion of the obj"""
+        return "{}/{}".format(self.width, self.width)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
