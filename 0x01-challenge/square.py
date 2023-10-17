28dab3ab920cb13491f0884858be4eae87d42892
MahmoudHamdy <hamdy.xd55@gmail.com>
2023-10-18 02:02:04 +0300

    Fix square class that takes a width and a height

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
+""" Square class """
+
 
 class Square():
-    """Class Square"""
-    
+    """ Square class """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
-        """init function"""
-        self.width = self.height = 0
+        """ Constructor """
+        self.width = 0
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        """ Permiter of the square """
-        return (self.width * 2) + (self.width * 2)
+    def permiter_of_my_square(self):
+        """ Perimeter of the square """
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """string representaion of the obj"""
-        return "{}/{}".format(self.width, self.width)
+        """ Print the square """
+        return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
     s = Square(width=12)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
